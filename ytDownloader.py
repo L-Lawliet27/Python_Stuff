b2d02e913cf42d4097c4d7e5fdcf36ad1efd3bb8
L-Lawliet27 <andressalazarcolina@gmail>
2023-03-15 17:30:12 +0100

    Tweaks

--- ytDownloader.py
+++ ytDownloader.py
@@ -1,14 +1,19 @@
 from pytube import Playlist, YouTube
+from moviepy.video.io.VideoFileClip import VideoFileClip
 from os import path, makedirs, rmdir
 from sys import argv
-from time import sleep
 
 videoPath = "/Users/Andres/Movies/"
+defaultFolderName = "YouTubeDownloader"
 
-
-def getDownloadFolder(name="YouTubeDownloader"):
+usage = "\nUsage: [python3] ytDownloader.py <option> <url>\n\n" + \
+    "-v:  download single video\n" + \
+    "-p:  download playlist\n" +\
+    "-h:  this help message\n\n"
+ 
+def getDownloadFolder(name=defaultFolderName):
     newDir = path.join(videoPath, name)
-    makedirs(newDir, exist_ok=True)
+    makedirs(newDir, exist_ok=True, mode=0o777)
     return newDir
 
 
@@ -20,9 +25,21 @@ def downloadPlaylist(url):
         downloadVideo(url=None, video=v, folder=downloadFolder)
 
 
+def cleanVideoTitle(videoTitle):
+    title = videoTitle.replace(" ", "")
+    if ":" in title:
+        title=title.replace(":", "_")
+    if "(" in title:
+         title=title.strip().split("(")[0].rstrip()
+    if "[" in title:
+        title=title.strip().split("(")[0].rstrip()
+    return title
+
+
 def downloadVideo(url=None, video=None, folder=None):
     if url is not None:
         video = YouTube(url)
+        video.title = cleanVideoTitle(video.title)
         folder = getDownloadFolder()
 
     resolution = video.streams.get_highest_resolution()  # Only goes up to 720p
@@ -39,10 +56,9 @@ def downloadVideo(url=None, video=None, folder=None):
 def main():
     try:
         if len(argv) > 3:
-            raise Exception("The usage must be: downloadYT.py LINK")
+            raise Exception(f"The usage must be: {usage}")
         if argv[1] == "-h" or "":
-            print(
-                "\nUsage: [python3] ytDownloader.py <option> <url>\n\n-v: download single video\n-p: download playlist\n-h: this help message\n\n")
+            print(usage)
             exit()
         if not argv[2]:
             raise Exception("The Link Field cannot be empty\n")
@@ -53,7 +69,8 @@ def main():
             downloadVideo(url)
         elif argv[1] == "-p":
             downloadPlaylist(url)
-
+        else:
+            raise Exception("Invalid Option\n")
     except KeyboardInterrupt:
         print("Exited\n")
     except Exception as e:
