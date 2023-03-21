5807b555dbac3c28e1e7f62517488b23b6c73254
L-Lawliet27 <andressalazarcolina@gmail>
2023-03-21 21:46:17 +0100

    Added Single Upload

--- eBookPriceNotifier/sendToKindle.py
+++ eBookPriceNotifier/sendToKindle.py
@@ -2,39 +2,54 @@ import smtplib
 import json
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
-from os import scandir, path, makedirs, replace
+from os import scandir, path, makedirs, replace, chdir
+from sys import argv
 
-kindleFolder = "/Users/Andres/Documents/Kindle/"
+
+kindleFolder = f"/Users/Andres/Documents/Kindle/"
+downloadsFolder = "Downloads/"
+downloadsPath = f"/Users/Andres/Downloads/"
 ebookTypes = ('.epub', '.mobi', '.pdf')
+
 
 def getSubtype(fileExt):
     return path.splitext(fileExt)[1][1:]
 
 
-def scanAndSort(msg):
-    directory = path.join(kindleFolder, "SentEbooks")
-    makedirs(directory, exist_ok=True)
+def attachEbook(fileName, filePath, fileExt, dir, msg):
+    with open(filePath, "rb") as b:
+        ebook_data = b.read()
+        ebook = MIMEApplication(ebook_data, Name=fileName,
+                                _subtype=getSubtype(fileExt))
+        ebook.add_header('Content-Disposition', 'attachment',
+                         filename=ebook.get_filename())
+        msg.attach(ebook)
+        newPath = path.join(dir, fileName)
+        replace(filePath, newPath)
+
+
+def scanAndSort(msg, directory):
     for file in scandir(kindleFolder):
         if file.is_file():
             fileExt = path.splitext(file.name)[1].lower()
             if fileExt in ebookTypes:
-                with open(file.path, "rb") as b:
-                    ebook_data = b.read()
-                ebook = MIMEApplication(ebook_data, Name=file.name, _subtype=getSubtype(fileExt))
-                ebook.add_header('Content-Disposition', 'attachment', filename=ebook.get_filename())
-                msg.attach(ebook)
-                newPath = path.join(directory, file.name)
-                replace(file.path, newPath)
+                attachEbook(fileName=file.name, filePath=file.path,
+                            fileExt=fileExt, dir=directory, msg=msg)
 
 
-
-def sendToKindle(kindleEmail, email, passwd):
+def sendToKindle(kindleEmail, email, passwd, oneEbook=None):
+    directory = path.join(kindleFolder, "SentEbooks")
+    makedirs(directory, exist_ok=True)
     msg = MIMEMultipart()
-    msg['Subject']="eBook(s) Sent"
-    msg['To']=kindleEmail
-    msg['From']=email
+    msg['Subject'] = "eBook(s) Sent"
+    msg['To'] = kindleEmail
+    msg['From'] = email
 
-    scanAndSort(msg)
+    if oneEbook is None:
+        scanAndSort(msg, directory)
+    else:
+        attachEbook(fileName=oneEbook[0], filePath=oneEbook[1],
+                    fileExt=oneEbook[2], dir=directory, msg=msg)
 
     with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
         smtp.starttls()
@@ -43,15 +58,33 @@ def sendToKindle(kindleEmail, email, passwd):
         smtp.quit()
 
 
-
 def main():
-    info = json.load(open('info.json'))
-    kindleEmail=info.get("kindle_email")
-    email=info.get("my_email")
-    passwd=info.get("sender_pswd")
-    sendToKindle(kindleEmail, email, passwd)
+    chdir(path.dirname(path.realpath(__file__)))
+    info = json.load(open("info.json"))
+    kindleEmail = info.get("kindle_email")
+    email = info.get("my_email")
+    passwd = info.get("sender_pswd")
+
+    if len(argv) == 3:
+        if argv[1] != "-s":
+            print(
+                "Usage: sendToKindle.py [-s <path to file in \"Downloads\" folder or file name>]\n\n")
+            exit()
+
+        base_name = path.split(argv[2])[1].strip()
+        absPath = path.join(downloadsPath, base_name)
+
+        if path.isfile(absPath):
+            fileExt = path.splitext(base_name)[1].lower()
+            if fileExt in ebookTypes:
+                sendToKindle(kindleEmail, email, passwd,
+                             oneEbook=(base_name, absPath, fileExt))
+            else:
+                print("This is not a valid file — Valid files: .epub, .mobi, .pdf\n\n")
+                exit()
+        else:
+            sendToKindle(kindleEmail, email, passwd)
 
 
 if __name__ == '__main__':
     main()
-    
